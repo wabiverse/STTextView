797932690caf153efb162e5458d8042b2a32e3b8
furby™ <devs@wabi.foundation>
2023-07-04 03:30:46 -0500

    bump to swift 5.9

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
   name: "STTextView",
-  platforms: [.macOS(.v12)],
+  platforms: [.macOS(.v13)],
   products: [
     .library(
       name: "STTextView",
